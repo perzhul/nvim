96029472db2a7d9220e804818a86a08c70c70a3a
Daniel <perzhul.works@gmail.com>
2024-08-19 14:43:15 +0300

    feat: add oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  config = function()
+    require('oil').setup {
+      delete_to_trash = true,
+      use_icons = true,
+      use_default_keymaps = true,
+      lsp_file_methods = {
+        'textDocument/codeAction',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      git = {
+        show_untracked = true,
+        show_ignored = true,
+      },
+    }
+  end,
+}
