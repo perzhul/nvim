56b734b8e9b272db90ff01137aa020c601cb9268
polukarp <perzhul.works@gmail.com>
2024-08-25 17:33:12 +0300

    chore: add language server for toml files

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -86,6 +86,7 @@ return {
         pyright = {},
         rust_analyzer = {},
         cssls = {},
+        taplo = {},
         tsserver = {
           commands = {
             OrganizeImports = {
