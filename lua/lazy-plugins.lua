7847e4aa287d4a2dd16c066385ef633baa275b64
polukarp <perzhul.works@gmail.com>
2024-08-30 20:00:03 +0300

    chore: remove indent_line plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -29,8 +29,6 @@ require('lazy').setup({
 
   require 'kickstart.plugins.debug',
 
-  require 'kickstart.plugins.indent_line',
-
   require 'kickstart.plugins.lint',
 
   require 'kickstart.plugins.autopairs',
