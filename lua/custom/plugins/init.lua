52d46d8efcbabb22cfc1c33ee6f7cdf01f3f8474
polukarp <perzhul.works@gmail.com>
2024-08-30 19:55:23 +0300

    feat: add undotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,5 +5,11 @@ return {
   'eandrju/cellular-automaton.nvim',
   'ThePrimeagen/vim-be-good',
   'saecki/live-rename.nvim',
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '\\', vim.cmd.UndotreeToggle)
+    end,
+  },
   -- 'github/copilot.vim',
 }
