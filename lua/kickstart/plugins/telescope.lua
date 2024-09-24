2cddd60856b3a77c055e37a44a4547ac1a7eb841
polukarp <perzhul.works@gmail.com>
2024-09-24 22:35:09 +0300

    chore: add smart_open to telescope

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
 
     config = function()
       local gfh_actions = require('telescope').extensions.git_file_history.actions
+
       local actions = require 'telescope.actions'
 
       local trouble = require 'trouble'
@@ -45,6 +46,16 @@ return {
           },
         },
         extensions = {
+          smart_open = {
+            cwd_only = true,
+            filename_first = false,
+            ignore_patterns = {
+              '*.temp/*',
+              'index.ts',
+              'ns.ts',
+            },
+            match_algorithm = 'fzf',
+          },
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
@@ -75,7 +86,7 @@ return {
       map('n', '<leader>gh', '<cmd>Telescope git_file_history<CR>', { desc = 'Open git file history' })
       map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -83,7 +94,7 @@ return {
       map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       map('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch [C]olorscheme' })
-      map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      map('n', '<leader><leader>', '<cmd>Telescope smart_open theme=dropdown previewer=false<cr>', { desc = 'Find Files (Smart open)' })
 
       map('n', '<leader>/', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
