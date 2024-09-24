0b2dc8b21cb1f64cd0d30de51630ee4490941fba
polukarp <perzhul.works@gmail.com>
2024-09-24 23:19:52 +0300

    chore: simplify diffview bindings

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,7 +1,7 @@
 return {
   'sindrets/diffview.nvim',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>dvo', '<cmd>DiffviewOpen<CR>', { desc = '[D]iff[V]iew [O]pen' })
-    vim.api.nvim_set_keymap('n', '<leader>dvc', '<cmd>DiffviewClose<CR>', { desc = '[D]iff[V]iew [C]lose' })
+    vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>DiffviewOpen<CR>', { desc = '[D]iff [O]pen' })
+    vim.api.nvim_set_keymap('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = '[D]iff [C]lose' })
   end,
 }
--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -19,3 +19,4 @@ return {
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
+
