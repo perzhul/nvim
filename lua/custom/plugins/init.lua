b88847eac2c6e158458e5408e54d6656d16d7d8e
polukarp <perzhul.works@gmail.com>
2024-10-19 15:08:07 +0500

    feat: nice theme switcher

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,11 @@
 return {
+  {
+    'andrew-george/telescope-themes',
+    config = function()
+      require('telescope').load_extension 'themes'
+      vim.keymap.set('n', '<leader>th', ':Telescope themes<CR>', { noremap = true, silent = true, desc = 'Theme Switcher' })
+    end,
+  },
   { 'artemave/workspace-diagnostics.nvim', lazy = true },
   {
     'saecki/live-rename.nvim',
