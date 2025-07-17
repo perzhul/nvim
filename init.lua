71ad13a31de4150622ab39fd638b9638e4b35a39
perzhul <perzhul.works@gmail.com>
2025-07-17 10:16:35 +0300

    refactor: remove non-essential plugins

--- init.lua
+++ init.lua
@@ -1,53 +1,29 @@
 require 'options'
 require 'keymaps'
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'http',
-  callback = function()
-    vim.bo.commentstring = '# %s' -- helpful for `rest.nvim`
-  end,
-})
-
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   { 'tpope/vim-sleuth', event = 'BufReadPost' },
-  { 'echasnovski/mini.ai', version = '*', opts = {}, event = 'VeryLazy' }, -- cool plugin, doesn't have nothing to do with AI, don't remove
-  {
-    'saecki/live-rename.nvim',
-    event = 'VeryLazy',
-    config = function()
-      vim.keymap.set('n', '<leader>r', require('live-rename').rename, { desc = 'LSP rename' })
-    end,
-  },
-  { 'windwp/nvim-ts-autotag', ft = { 'html', 'htmldjango', 'xml', 'javascriptreact', 'typescriptreact' }, opts = {} },
-  { 'tpope/vim-fugitive', cmd = { 'Git', 'Gdiffsplit', 'Gvdiffsplit', 'Gread', 'Gwrite' } },
+  { 'echasnovski/mini.ai', opts = {}, event = 'VeryLazy' },
   { 'tpope/vim-unimpaired', event = 'BufReadPost' },
-  {
-    'kylechui/nvim-surround',
-    version = '^3.0.0',
-    event = 'InsertEnter',
-    opts = {},
-  },
+  { 'kylechui/nvim-surround', event = 'InsertEnter', opts = {} },
   { 'artemave/workspace-diagnostics.nvim' },
   { 'eandrju/cellular-automaton.nvim' },
   { 'wakatime/vim-wakatime', lazy = false, opts = {} },
-  { 'folke/zen-mode.nvim', opts = {
-    window = {
-      width = 120,
-    },
-  } },
-  { 'ellisonleao/gruvbox.nvim' },
-  { 'jaredgorski/Mies.vim' },
-  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
-  { 'forest-nvim/sequoia.nvim' },
-  { 'rose-pine/neovim' },
-  { 'rebelot/kanagawa.nvim' },
+  { 'folke/zen-mode.nvim', opts = { window = { width = 120 } } },
+  {
+    'jaredgorski/Mies.vim',
+    config = function()
+      vim.cmd [[set background=light]]
+      vim.cmd [[colorscheme mies]]
+    end,
+  },
 }
 
 local plugins_dir = vim.fn.stdpath 'config' .. '/lua/plugins'
@@ -59,12 +35,3 @@ for _, file in ipairs(vim.fn.readdir(plugins_dir)) do
 end
 
 require('lazy').setup(plugins)
-
-vim.cmd.colorscheme 'github_dark'
-
--- local hr = tonumber(os.date('%H', os.time()))
--- if hr > 6 and hr < 20 then
---   vim.o.background = 'light'
--- else
---   vim.opt.background = 'dark'
--- end
